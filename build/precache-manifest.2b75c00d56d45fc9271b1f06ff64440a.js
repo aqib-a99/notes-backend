f6263e712b697b8f1a8fce216340e6f5b89f0228
insignificanthomosapien <aja6082@psu.edu>
2020-06-17 00:41:12 +0530

    build production for frontend added

---
+++ build/precache-manifest.2b75c00d56d45fc9271b1f06ff64440a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b27a6e04c8732d01a4ff6c403ffe54ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0756fdebd5b7e000dab7",
+    "url": "/static/css/main.f935c275.chunk.css"
+  },
+  {
+    "revision": "71aece79297c53235b5b",
+    "url": "/static/js/2.16519f6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.16519f6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0756fdebd5b7e000dab7",
+    "url": "/static/js/main.6340a337.chunk.js"
+  },
+  {
+    "revision": "080c736444cb4e773aaf",
+    "url": "/static/js/runtime-main.17595985.js"
+  }
+]);
---
+++ build/precache-manifest.a2779580d8e3a840ade8ea37d1a629e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "858ce1b6cdc294e0a9947471155db02e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c6de8edb5c7d95e4b5c",
+    "url": "/static/css/main.f935c275.chunk.css"
+  },
+  {
+    "revision": "71aece79297c53235b5b",
+    "url": "/static/js/2.16519f6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.16519f6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c6de8edb5c7d95e4b5c",
+    "url": "/static/js/main.8ec5b640.chunk.js"
+  },
+  {
+    "revision": "080c736444cb4e773aaf",
+    "url": "/static/js/runtime-main.17595985.js"
+  }
+]);
